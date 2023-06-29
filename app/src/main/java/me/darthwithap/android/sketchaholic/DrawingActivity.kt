951e827e3bdea38a301609c6b977ea8147ea7cc4
patthhar <parthdroidworld@gmail.com>
2023-06-29 19:48:56 +0530

    Renamed MainActivity to SetupActivity and added DrawingActivity

--- app/src/main/java/me/darthwithap/android/sketchaholic/DrawingActivity.kt
+++ app/src/main/java/me/darthwithap/android/sketchaholic/DrawingActivity.kt
@@ -3,9 +3,9 @@ package me.darthwithap.android.sketchaholic
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class DrawingActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    setContentView(R.layout.activity_drawing)
   }
 }
---
+++ app/src/main/java/me/darthwithap/android/sketchaholic/ui/setup/SetupActivity.kt
@@ -0,0 +1,12 @@
+package me.darthwithap.android.sketchaholic.ui.setup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.darthwithap.android.sketchaholic.R
+
+class SetupActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_setup)
+  }
+}
