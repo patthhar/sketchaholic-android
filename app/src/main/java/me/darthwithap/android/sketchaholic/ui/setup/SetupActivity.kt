5ddd4f65e5429196c313e11dfa959e49808eaede
patthhar <parthdroidworld@gmail.com>
2023-06-30 01:17:50 +0530

    Default night mode disabled

--- app/src/main/java/me/darthwithap/android/sketchaholic/ui/setup/SetupActivity.kt
+++ app/src/main/java/me/darthwithap/android/sketchaholic/ui/setup/SetupActivity.kt
@@ -2,11 +2,16 @@ package me.darthwithap.android.sketchaholic.ui.setup
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import me.darthwithap.android.sketchaholic.R
+import androidx.appcompat.app.AppCompatDelegate
+import me.darthwithap.android.sketchaholic.databinding.ActivitySetupBinding
 
 class SetupActivity : AppCompatActivity() {
+
+  private lateinit var binding: ActivitySetupBinding
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_setup)
+    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    binding = ActivitySetupBinding.inflate(layoutInflater)
+    setContentView(binding.root)
   }
 }
