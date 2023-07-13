119112cec1177ea9bbe32521f11e06543b1fe841
patthhar <parthdroidworld@gmail.com>
2023-07-13 13:57:21 +0530

    DrawingApi interface and provide added in app module

---
+++ app/src/main/java/me/darthwithap/android/sketchaholic/data/remote/websockets/DrawingApi.kt
@@ -0,0 +1,19 @@
+package me.darthwithap.android.sketchaholic.data.remote.websockets
+
+import com.tinder.scarlet.WebSocket
+import com.tinder.scarlet.ws.Receive
+import com.tinder.scarlet.ws.Send
+import kotlinx.coroutines.flow.Flow
+import me.darthwithap.android.sketchaholic.data.remote.websockets.models.BaseModel
+
+
+interface DrawingApi {
+  @Receive
+  fun observeWebSocketEvents(): Flow<WebSocket.Event>
+
+  @Send
+  fun sendBaseModel(baseModel: BaseModel): Boolean
+
+  @Receive
+  fun observeBaseModels(): Flow<BaseModel>
+}
--- app/src/main/java/me/darthwithap/android/sketchaholic/di/AppModule.kt
+++ app/src/main/java/me/darthwithap/android/sketchaholic/di/AppModule.kt
@@ -3,6 +3,9 @@ package me.darthwithap.android.sketchaholic.di
 import android.app.Application
 import android.content.Context
 import com.google.gson.Gson
+import com.tinder.scarlet.Scarlet
+import com.tinder.scarlet.retry.LinearBackoffStrategy
+import com.tinder.scarlet.websocket.okhttp.newWebSocketFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,10 +15,15 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import me.darthwithap.android.sketchaholic.data.remote.SetupApi
+import me.darthwithap.android.sketchaholic.data.remote.websockets.CustomGsonMessageAdapter
+import me.darthwithap.android.sketchaholic.data.remote.websockets.DrawingApi
+import me.darthwithap.android.sketchaholic.data.remote.websockets.FlowStreamAdapter
 import me.darthwithap.android.sketchaholic.data.repository.SetupRepository
 import me.darthwithap.android.sketchaholic.data.repository.SetupRepositoryImpl
 import me.darthwithap.android.sketchaholic.util.Constants.HTTP_BASE_URL_LOCAL_HOST
 import me.darthwithap.android.sketchaholic.util.Constants.KEY_CLIENT_ID_QUERY_PARAM
+import me.darthwithap.android.sketchaholic.util.Constants.RECONNECT_INTERVAL
+import me.darthwithap.android.sketchaholic.util.Constants.WEBSOCKET_BASE_URL
 import me.darthwithap.android.sketchaholic.util.DispatcherProvider
 import me.darthwithap.android.sketchaholic.util.clientId
 import me.darthwithap.android.sketchaholic.util.dataStore
@@ -38,20 +46,15 @@ object AppModule {
   @Singleton
   @Provides
   fun provideOkHttpClient(clientId: String): OkHttpClient {
-    return OkHttpClient.Builder()
-      .addInterceptor { chain ->
-        val request = chain.request().newBuilder()
-          .url(
-            chain.request().url.newBuilder()
-              .addQueryParameter(KEY_CLIENT_ID_QUERY_PARAM, clientId)
-              .build()
-          )
+    return OkHttpClient.Builder().addInterceptor { chain ->
+      val request = chain.request().newBuilder().url(
+        chain.request().url.newBuilder().addQueryParameter(KEY_CLIENT_ID_QUERY_PARAM, clientId)
           .build()
-        chain.proceed(request)
-      }
-      .addInterceptor(HttpLoggingInterceptor().apply {
-        level = HttpLoggingInterceptor.Level.BODY
-      }).build()
+      ).build()
+      chain.proceed(request)
+    }.addInterceptor(HttpLoggingInterceptor().apply {
+      level = HttpLoggingInterceptor.Level.BODY
+    }).build()
   }
 
   @Provides
@@ -65,11 +68,8 @@ object AppModule {
   @Singleton
   @Provides
   fun provideSetupApi(okHttpClient: OkHttpClient): SetupApi {
-    return Retrofit.Builder()
-      .baseUrl(HTTP_BASE_URL_LOCAL_HOST)
-      .addConverterFactory(GsonConverterFactory.create())
-      .client(okHttpClient)
-      .build()
+    return Retrofit.Builder().baseUrl(HTTP_BASE_URL_LOCAL_HOST)
+      .addConverterFactory(GsonConverterFactory.create()).client(okHttpClient).build()
       .create(SetupApi::class.java)
   }
 
@@ -94,13 +94,22 @@ object AppModule {
 
   @Provides
   @Singleton
+  fun provideDrawingApi(
+    app: Application, okHttpClient: OkHttpClient, gson: Gson
+  ): DrawingApi {
+    return Scarlet.Builder().backoffStrategy(LinearBackoffStrategy(RECONNECT_INTERVAL))
+      .webSocketFactory(okHttpClient.newWebSocketFactory(WEBSOCKET_BASE_URL))
+      .addStreamAdapterFactory(FlowStreamAdapter.Factory)
+      .addMessageAdapterFactory(CustomGsonMessageAdapter.Factory(gson)).build().create()
+  }
+
+  @Provides
+  @Singleton
   fun provideSetupRepository(
-    setupApi: SetupApi,
-    context: Context
+    setupApi: SetupApi, context: Context
   ): SetupRepository {
     return SetupRepositoryImpl(
-      setupApi,
-      context
+      setupApi, context
     )
   }
 }
